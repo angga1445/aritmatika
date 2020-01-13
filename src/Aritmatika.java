9f8891c13de0ad5272bdd7a1d5ec8d5433e239cd
Angga <anggaherlangga82@yahoo.com>
2020-01-13 12:39:42 +0700

    Program Aritmatika

---
+++ src/Aritmatika.java
@@ -0,0 +1,19 @@
+public class Aritmatika {
+    
+    public static void main(String[] args) {
+        //deklarasi variable
+        int a=10;
+        int b=5;
+        double tambah,bagi,kali,kurang;
+        
+        //rumus aritmatika
+        tambah= a+b;
+        kurang= a-b;
+        kali= a*b;
+        bagi= a/b;
+        
+        //output nilai
+        System.out.println("Nilai penjumlahan: "+tambah+"\n"+"Nilai pengurangan: "+kurang+"\n"+"Nilai perkalian: "+kali+"\n"+"Nilai pembagian: "+bagi);
+    }
+    
+}
